c0083e2ac5505d2b8ef5b715a7026badf4b41039
GDZ0mb1e <seantsai91@gmail.com>
2025-11-27 20:32:37 +0800

    Keyword

---
+++ src/main/java/com/example/boogle/keywordsearching/WebPage.java
@@ -0,0 +1,32 @@
+package com.example.boogle.keywordsearching;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class WebPage {
+	public String url;
+	public String name;
+	public WordCounter counter;
+	public double score;
+
+	
+	public WebPage(String url,String name){
+		this.url = url;
+		this.name = name;
+		this.counter = new WordCounter(url);	
+	}
+	
+	public void setScore(ArrayList<Keyword> keywords) throws IOException{
+		// YOUR TURN
+		// 1. calculate the score of this webPage
+		this.score = 0.0;
+		for(int i=0;i<keywords.size();i++) {
+			try{
+				int number = counter.countKeyword(keywords.get(i).name);
+				this.score += number * keywords.get(i).weight;
+			}catch(IOException e){}
+		}
+
+	}
+	
+}
